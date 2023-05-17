4cb01f1f527ec21b5e80f228f9f31295c0bb218a
cjy <c0512jy@naver.com>
2023-05-17 10:11:13 +0900

    refactor

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # soundFilter
 Filtering noise(removing hum, removing electric noise)
+
+data폴더 : 원본 및 노이즈 웨이브 파일
+result폴더 : 노이즈 제거 웨이브 파일
--- filter.py
+++ filter.py
@@ -3,6 +3,7 @@ from scipy import signal
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import librosa
 
 #그래프를 그려줄 함수
 def show_plot(files):
@@ -14,29 +15,29 @@ def show_plot(files):
     for idx, file in enumerate(files) :
         full_path = path + '\\data\\' + file
         wav = full_path # Original file
-        # (file_dir, file_id) = os.path.split(wav)
-
-        # print("Path : ", file_dir)
-        # print("Name : ", file_id)
-
-        sample_rate, data = wavfile.read(wav) # sr : sampling rate, x : wave data array
-
-        # print("Sample rate:{0}, data size:{1}, duration:{2} seconds".format(sample_rate,data.shape,len(data)/sample_rate))
-
+        
+        sample_rate, data = wavfile.read(wav) # sample_rate : sampling rate, data : wave data array
+        #위 wavfile.read() 대신 아래의 librosa.load()를 사용하면 정상적인 그래프 출력
+        #data, sample_rate = librosa.load(wav, sr=None)
+        
+        #시간축
         time = np.linspace(0, len(data)/sample_rate, len(data))
         
-        # axes[idx].figure(figsize=(20,10))
+        #시간, 진폭 그래프
         axes[idx][0].plot(time, data)
         axes[idx][0].set_ylabel("Amplitude")
         axes[idx][0].set_xlabel("Time[s]")
         axes[idx][0].set_title(file)
         
+        #고속 푸리에 변환
         fft = np.fft.fft(data)
-
+        #변환 배열 절댓값
         magnitude = np.abs(fft) 
 
         f = np.linspace(0, sample_rate, len(magnitude))
+        #진폭
         left_spectrum = magnitude[:int(len(magnitude)/2)]
+        #주파수
         left_f = f[:int(len(magnitude)/2)]
 
         axes[idx][1].plot(left_f, left_spectrum)
@@ -47,55 +48,61 @@ def show_plot(files):
     plt.tight_layout()
     plt.show()
 
-files = ['test.wav', 'humtest.wav', 'electest.wav']
-show_plot(files)
-
+#high pass filter func
 def highpass_filter(data, high, fs, order):
     b = signal.firwin(order, cutoff=high, fs=fs , pass_zero='highpass')
     y = signal.lfilter(b, [1.0], data)
     return y
 
+#low pass filter func
 def lowpass_filter(data, low, fs, order):
     b = signal.firwin(order, cutoff=low, fs=fs , pass_zero='lowpass')
     y = signal.lfilter(b, [1.0], data)
     return y
 
+#humtest.wav파일에 적용
 def hum():
     full_path = r'.\data\humtest.wav'
     wav = full_path # Original file
-    sample_rate, data = wavfile.read(wav) # sr : sampling rate, x : wave data array
+    sample_rate, data = wavfile.read(wav) #sample_rate : sampling rate, data : wave data array
     
+    # humtest.wav 800Hz highpass filter를 적용
     hum_filtered_data = highpass_filter(data, 800 , sample_rate, 101)
-    hum_filtered_data2 = lowpass_filter(data, 0.000001 , sample_rate, 101)
-    hum_filtered_file = r'.\result\humfilter.wav'
-    hum_filtered_file2 = r'.\result\humfilter2.wav'
+    #저장할 파일 이름
+    hum_filtered_file = r'.\result\humtest_clean.wav'
+    #파일 저장
     wavfile.write(hum_filtered_file, sample_rate, hum_filtered_data.astype(np.int16))
-    wavfile.write(hum_filtered_file2, sample_rate, hum_filtered_data2.astype(np.int16))
     
-hum()
-
-# highpass filter를 사용하면 removing hum이 어느 정도 가능하다.
-# 참고 https://github.com/hyorea1/wavFile_preprocessing/blob/main/waveFilter.ipynb
-#       https://eclipse360.tistory.com/114
-#       https://coding-yoon.tistory.com/23
-
-# electest는 어떻게 없애야 하나
-
+#electest.wav파일에 적용
 def elec():
     full_path = r'.\data\electest.wav'
     wav = full_path # Original file
-    sample_rate, data = wavfile.read(wav) # sr : sampling rate, x : wave data array
+    sample_rate, data = wavfile.read(wav) # sample_rate : sampling rate, data : wave data array
     
-    elec_filtered_data = highpass_filter(data, 7000 , sample_rate, 101)
-    elec_filtered_data2 = lowpass_filter(data, 3000 , sample_rate, 101)
-    elec_filtered_file = r'.\result\elecfilter.wav'
-    elec_filtered_file2 = r'.\result\elecfilter2.wav'
-    elec_filtered_data *= 10000
-    elec_filtered_data2 *= 10000
-    wavfile.write(elec_filtered_file, sample_rate, elec_filtered_data.astype(np.int16))
-    wavfile.write(elec_filtered_file2, sample_rate, elec_filtered_data2.astype(np.int16))
+    # electest.wav에 3000 lowpass filter를 적용
+    elec_filtered_data = lowpass_filter(data, 3000 , sample_rate, 101)
+    #저장할 파일 이름
+    elec_filtered_file = r'.\result\electest_clean.wav'
     #소리가 너무 작아서 amplitude 10000배
-    data *= 10000
-    wavfile.write(r'.\eleczz.wav',sample_rate,data.astype(np.int16))
-    
+    elec_filtered_data *= 10000
+    #파일 저장
+    wavfile.write(elec_filtered_file, sample_rate, elec_filtered_data.astype(np.int16))
+
+# MSE 계산 함수
+def calc_mse(file):
+    original_data, sample_rate_original = librosa.load('data\\test.wav', sr=None)
+    modified_data, sample_rate_hum = librosa.load(file, sr=None)
+
+    mse = np.mean((original_data - modified_data) ** 2)
+ 
+    return mse
+
+# Original files
+files = ['test.wav', 'humtest.wav', 'electest.wav']
+show_plot(files)
+
+hum()
 elec()
+
+print("Mean Square Error between humtest_clean and test: ", calc_mse('result\humfilter.wav'))
+print("Mean Square Error between electest_clean and test: ", calc_mse('result\elecfilter2.wav'))
